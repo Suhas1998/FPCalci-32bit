5abac89a445e53e61e6d2d83c1370a54448c9bc9
Suhas1998 <suhas.edu23@gmail.com>
2020-03-27 15:17:56 +0530

    Converted to binary

--- calculate.js
+++ calculate.js
@@ -1,13 +1,31 @@
-const calculator = {
-  displayValue: '0',
-  firstOperand: null,
-  waitingForSecondOperand: false,
-  operator: null,
-};
 
-function addNumbers(){
-  let x = document.getElementById('num1').value
-  let y = document.getElementById('num2').value
-  var sum = parseInt(x) + parseInt(y)
-  document.getElementById('result').value = sum
+function findbin(number){
+  // Convert int part to binary
+  // Convert decimal part to binary
+  // Convert to scientific form
+  var bytes = [].map.call(new Uint8Array((new Float64Array([number])).buffer), function(b) {
+          for (var s = b.toString(2), i = s.length; i < 8; i++) {
+              s = "0" + s;
+          }
+          return s;
+      });
+  bytes = bytes.reverse();
+  var binary = bytes.join();
+  console.log(binary);
+}
+
+function evaluate(num1, num2, op){
+  var bin1, bin2;
+  bin1 = findbin(num1);
+  bin2 = findbin(num2);
+  return 0;
+}
+
+function calculate(){
+  let x = document.getElementById('num1').value;
+  let y = document.getElementById('num2').value;
+  let operator = document.getElementById('operator').value;
+  // Evaluate the result
+  var result = evaluate(x, y, operator);
+  // Write the values as output
 }
