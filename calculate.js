8d1a6c10a119484a53603d7295587aec91a52877
Suhas1998 <suhas.edu23@gmail.com>
2020-03-30 21:52:29 +0530

    Rounded to 32bit fp number from 64bit

--- calculate.js
+++ calculate.js
@@ -1,45 +1,60 @@
+/* For testing:
+0.125 : 0011111111000000000000000000000000000000000000000000000000000000
+0.2   : 0011111111001001100110011001100110011001100110011001100110011010  */
+
 // This is bit wise representation of Floating point number.
+
+var mantissaBits = 23;
+var possibleMantissas = Math.pow(2, mantissaBits);
+
 function findBin(number){
-  var bytes = [].map.call(new Uint8Array((new Float64Array([number])).buffer), function(b) {
-          // After converting to Uint8Array, the 64 bits of the flat value is stored as an array of length 8 with each element having 8 bits represented as an int. Thus now converting each of the 8 bit int number as binary.
-          for (var s = b.toString(2), i = s.length; i < 8; i++) {
-            s = "0" + s; //Adding extra 0s to make it string of length 8.
-          }
-          return s;
-      });
-  bytes = bytes.reverse();
-  var binary = bytes.join("");
-  console.log(new Uint8Array((new Float64Array([number])).buffer))
-  return binary;
+  // Before Rounding
+  var float = new Float64Array([number]);
+  var bytes = new Uint8Array(float.buffer);
+  var sign = bytes[7]>>7;
+  var exponent = ((bytes[7] & 0x7f) << 4 | bytes[6] >> 4) - 0x3ff;
+  bytes[7] = 0x3f;
+  bytes[6] |= 0xf0;
+  var mantissa = float[0];
+  // Didnt bias the exponent yet
+  console.log(sign+" "+exponent+" "+mantissa);
+
+  // Rounding the number:
+  var mantissaRounded = Math.floor(mantissa * possibleMantissas) / possibleMantissas;
+  var numberRounded = (sign ? -1 : 1) * Math.pow(2, exponent) * mantissaRounded;
+
+  var floatRounded = new Float64Array([numberRounded]);
+  var bytesRounded = new Uint8Array(floatRounded.buffer);
+  var signRounded= bytesRounded[7]>>7;
+  var exponentRounded = ((bytesRounded[7] & 0x7f) << 4 | bytesRounded[6] >> 4) - 0x3ff;
+  bytesRounded[7] = 0x3f;
+  bytesRounded[6] |= 0xf0;
+  var mantissaRounded = floatRounded[0];
+  // Didnt bias the exponent yet
+  console.log(signRounded+" "+exponentRounded+" "+mantissaRounded);
+
+  return 0;
 }
 
+// This is the conversion of bit represented Floating point number to decimal number
 function findDec(number){
   if(number.length < 64){
-    return 0;
+    return 0; // Should improvise this later to add zeroes and carry on with the operation
   }
+  // Breaking the number into 8 parts
   var binary = number.match(/.{1,8}/g)
   var bytes = binary.reverse();
   for(var i = 0; i < 8; i++ ){
+    // Converting each of those 8 bits to integer
     bytes[i] = parseInt(bytes[i], 2);
   }
-
+  // Converting the array into Uint8Array
   var intArray = new Uint8Array(bytes)
   var floatArray = new Float64Array(intArray.buffer)
 
   return floatArray[0];
 }
 
-function evaluate(num1, num2, op){
-  var bin1, bin2;
-  // Convert the numbers into Bindary format with 64 bits
-  bin1 = findBin(num1);
-  bin2 = findBin(num2);
-
-  document.getElementById('bin1').value = bin1
-  document.getElementById('bin2').value = bin2
-
-  return 0;
-}
 
 function toBinary(){
   let x = document.getElementById('num1').value;
@@ -56,3 +71,24 @@ function toDecimal(){
   // Write the values as output
   document.getElementById('num2').value = result;
 }
+
+function fadd1(num1,num2) {
+  bin1 = findBin(num1);
+  bin2 = findBin(num2);
+
+  return fround((fround(num1,bits) + fround(num2,bits)),bits);
+}
+
+function calculate(){
+  let x = document.getElementById('num1').value;
+  let y = document.getElementById('num2').value;
+  // Later add code to get the operator (For now addition)
+  var result = fadd1(x,y)
+  document.getElementById('result').value = result
+
+
+}
+
+/* References:
+https://www.cs.uaf.edu/2000/fall/cs301/notes/notes/node51.html
+*/
