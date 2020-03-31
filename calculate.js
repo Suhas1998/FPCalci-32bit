5c0a295193a5df9b84c534095252a396c9743909
Suhas1998 <suhas.edu23@gmail.com>
2020-03-31 12:34:46 +0530

    4 Operations added

--- calculate.js
+++ calculate.js
@@ -11,14 +11,14 @@ function findBin(number){
   // Before Rounding
   var float = new Float64Array([number]);
   var bytes = new Uint8Array(float.buffer);
-  var bitwise = [].map.call(bytes, function(b){
+  var bit64 = [].map.call(bytes, function(b){
       for(var s = b.toString(2),i = s.length; i <8;i++ ){
         s = "0" + s;
       }
       return s;
-  })
-  bitwise = bitwise.reverse();
-  bitwise = bitwise.join("");
+  });
+  bit64 = bit64.reverse();
+  bit64 = bit64.join("");
 
   var sign = bytes[7]>>7;
   var exponent = ((bytes[7] & 0x7f) << 4 | bytes[6] >> 4) - 0x3ff;
@@ -26,12 +26,12 @@ function findBin(number){
   bytes[6] |= 0xf0;
   var mantissa = float[0];
   var value = (sign ? -1 : 1) * Math.pow(2,exponent) * mantissa;
-
+  // Later add 32 bit string as well
   return {
     sign: sign,
     exponent: exponent,
     mantissa: mantissa,
-    bitwise: bitwise,
+    bit64: bit64,
     value: value
   };
 }
@@ -80,24 +80,40 @@ function toDecimal(){
   document.getElementById('num2').value = result;
 }
 
-function fadd1(num1,num2) {
-  bin1 = findBin(num1);
-  bin2 = findBin(num2);
+function evaluate(num1,num2,operator) {
+  var bin1 = findBin(num1);
+  var bin2 = findBin(num2);
   bin1 = roundBin(bin1);
   bin2 = roundBin(bin2);
-  document.getElementById('bin1').value = bin1.bitwise;
-  document.getElementById('bin2').value = bin1.bitwise;
+  document.getElementById('bin1').value = bin1.bit64;
+  document.getElementById('bin2').value = bin1.bit64;
 
-  var sum = roundBin(findBin(bin1.value+bin2.value));
-  return sum.value;
+  var operation = {
+    add: function(num1,num2){
+          return roundBin(findBin(bin1.value+bin2.value));
+        },
+    subtract: function(num1,num2){
+          return roundBin(findBin(bin1.value-bin2.value));
+    },
+    multiply: function(num1,num2){
+          return roundBin(findBin(bin1.value*-bin2.value));
+    },
+    divide: function(num1,num2){
+          return roundBin(findBin(bin1.value/bin2.value));
+    },
+  };
+
+  var answer = operation[operator]();
+  document.getElementById('resultBin').value = answer.bit64;
+  return answer.value;
 }
 
 function calculate(){
   let x = document.getElementById('num1').value;
   let y = document.getElementById('num2').value;
   // Later add code to get the operator (For now addition)
-
-  var result = fadd1(x,y)
+  let operator = document.getElementById('operator').value;
+  var result = evaluate(x,y,operator)
   document.getElementById('result').value = result;
 }
 
